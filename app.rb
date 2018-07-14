760040e2e547fbb0a1c8a9318dfeb319267d519c
Hayden Betts <haydenbetts@gmail.com>
2018-07-14 17:35:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,6 +12,7 @@ class App < Sinatra::Base
   end
 
   post "/new" do
+    puppy = Puppy.new(params["name"], params["breed"], params["age"])
     @name = params["name"]
     @breed = params["breed"]
     @age = params["age"]
